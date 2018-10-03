39fc3539c2892a4a465d502c1472263a2da43a98
krHasan <hasanbappi@gmail.com>
2018-10-03 13:00:05 +0600

    1st commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dserver.port=$PORT -war target/Chit-Chat-0.0.1-SNAPSHOT.war
---
+++ src/main/java/com/chat/ChitChatApplication.java
@@ -0,0 +1,12 @@
+package com.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChitChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChitChatApplication.class, args);
+	}
+}
